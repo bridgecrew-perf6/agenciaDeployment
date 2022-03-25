3151a18edabf259e1700e1b022baaed04395d122
Efrain May <efrain_may_75@hotmail.com>
2022-03-24 23:42:56 -0600

    consultar base de datos desde una ulr especifica

--- controllers/paginasController.js
+++ controllers/paginasController.js
@@ -29,9 +29,27 @@ const paginaTestimoniales = (req, res) => {
     });
 }
 
+//Muestra un viaje por su slug
+const paginaDetalleViaje = async (req, res) => {
+    
+    const { slug } = req.params;
+
+    try {
+        const viaje = await Viaje.findOne({ where: { slug }});
+        
+        res.render('viaje', {
+            pagina: 'Informacion viaje',
+            viaje
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
 export {
     paginaInicio,
     paginaNosotros,
     paginaViajes,
-    paginaTestimoniales
+    paginaTestimoniales,
+    paginaDetalleViaje
 }
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,11 @@
 import express from 'express';
-import { paginaInicio, paginaNosotros, paginaTestimoniales, paginaViajes } from '../controllers/paginasController';
+import { 
+    paginaDetalleViaje, 
+    paginaInicio, 
+    paginaNosotros, 
+    paginaTestimoniales, 
+    paginaViajes 
+} from '../controllers/paginasController';
 
 const router = express.Router();
 
@@ -8,6 +14,8 @@ router.get('/', paginaInicio);
 router.get('/nosotros', paginaNosotros);
 
 router.get('/viajes', paginaViajes);
+
+router.get('/viajes/:slug', paginaDetalleViaje);
 
 router.get('/testimoniales', paginaTestimoniales);
 
